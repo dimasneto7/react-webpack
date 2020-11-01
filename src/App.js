0ff8e3cc8922c4c6058dd94e27bb4ab149323596
dimasneto <dimas_fcn@outlook.com>
2020-10-31 23:18:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+    return(
+        <div>
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                   
+                </ul>
+            </Header >
+        </div>
+    );
+}
+
+export default App;
